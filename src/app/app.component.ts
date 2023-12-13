a5a2e8d515ba06593f2332ebe9cbeb50bf51e75f
Daniel Frevel <daniel-frevel@web.de>
2023-12-13 23:23:37 +0100

    Setup + bubble sort

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,56 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { BehaviorSubject, Observable, ReplaySubject, generate } from 'rxjs';
+
+type SortItem = {
+  value: number;
+  color: string;
+};
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [CommonModule, RouterOutlet],
 })
 export class AppComponent {
-  title = 'AlgoViz';
+  sortItems = generateRandomArray();
+
+  bubbleSort() {
+    const arr = this.sortItems;
+    const n = arr.length;
+
+    for (let i = 0; i < n; i++) {
+      arr[i].color = 'red';
+      for (let j = 0; j < n - i - 1; j++) {
+        setTimeout(() => {
+          if (arr[j].value > arr[j + 1].value) {
+            const temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+          }
+        }, 100);
+      }
+      arr[i].color = 'white';
+    }
+
+    this.sortItems = arr;
+  }
+
+  randomize() {
+    this.sortItems = generateRandomArray();
+  }
+}
+
+function generateRandomArray(): SortItem[] {
+  const arr = [];
+  for (let i = 0; i < 300; i++) {
+    arr.push({
+      value: Math.floor(Math.random() * 1000),
+      color: 'white',
+    });
+  }
+  return arr;
 }
