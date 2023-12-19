c067be1ad73a4e9cee8f02ca60e1b21a77383913
Daniel Frevel <daniel-frevel@web.de>
2023-12-19 13:38:59 +0100

    funktionierende komische version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,21 +3,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {
   BehaviorSubject,
-  ReplaySubject,
-  combineLatest,
   concatMap,
   delay,
-  filter,
-  iif,
-  map,
   mergeMap,
   of,
   switchMap,
-  take,
-  takeUntil,
-  takeWhile,
   tap,
-  withLatestFrom,
 } from 'rxjs';
 
 type SortItem = {
@@ -33,96 +24,56 @@ type SortItem = {
   imports: [CommonModule, RouterOutlet],
 })
 export class AppComponent {
-  quickSort(arr: SortItem[]) {
-    let animationItems$ = new BehaviorSubject<SortItem[]>(arr);
+  quickSort() {
+    let arr = generateRandomArray();
     const n = arr.length;
-    animationItems$.next(arr);
-
-    for (let i = 0; i < n; i++) {
-      let min = i;
-      for (let j = i + 1; j < n; j++) {
-        if (arr[j].value < arr[min].value) {
-          min = j;
-        }
-        animationItems$.next([...arr]);
-      }
-      swap(arr, min, i);
-    }
-
-    return animationItems$;
+    this.animationItems$.next(arr);
   }
 
-  insertionSort(arr: SortItem[]) {
-    let animationItems$ = new BehaviorSubject<SortItem[]>(arr);
+  insertionSort() {
+    let arr = generateRandomArray();
     const n = arr.length;
-    animationItems$.next(arr);
+    this.animationItems$.next(arr);
     for (let i = 1; i < n; i++) {
       let j = i;
 
-      animationItems$.next([...arr]);
+      this.animationItems$.next([...arr]);
       while (j > 0 && arr[j - 1].value > arr[j].value) {
         swap(arr, j, j - 1);
         j = j - 1;
-        animationItems$.next([...arr]);
+        this.animationItems$.next([...arr]);
       }
     }
-    return animationItems$;
   }
+  animationItems$ = new BehaviorSubject<SortItem[]>(generateRandomArray());
 
-  speed$ = new BehaviorSubject<number>(1000000000000000000000000000000000);
-  size$ = new BehaviorSubject<number>(35);
-  breakSort$() {
-    this.sortAlgorithm$.next(([]) => {
-      return new BehaviorSubject<SortItem[]>([]);
-    });
-  }
-
-  sortAlgorithm$ = new ReplaySubject<
-    (arr: SortItem[]) => BehaviorSubject<SortItem[]>
-  >();
-
-  randomize$() {
-    this.sortAlgorithm$.pipe(take(1)).subscribe((algo) => {
-      this.sortAlgorithm$.next(algo);
-    });
-  }
-
-  sort_animation$ = this.sortAlgorithm$.pipe(
-    withLatestFrom(this.size$),
-    map(([sortAlgorithm, size]) => {
-      return sortAlgorithm(generateRandomArray(size));
-    }),
-    withLatestFrom(this.speed$),
-    concatMap(([items, speed]) => {
-      return items.pipe(delay(speed));
-    })
+  animation$ = this.animationItems$.pipe(
+    concatMap((item) => of(item).pipe(delay(1)))
   );
 
-  startBubbleSort() {
-    this.sortAlgorithm$.next(this.bubbleSort$);
+  randomize() {
+    this.animationItems$.next(generateRandomArray());
   }
 
-  bubbleSort$(arr: SortItem[]) {
-    let animationItems$ = new BehaviorSubject<SortItem[]>(arr);
+  bubbleSort() {
+    let arr = generateRandomArray();
     const n = arr.length;
-    animationItems$.next(arr);
+    this.animationItems$.next(arr);
     for (let i = 0; i < n; i++) {
       for (let j = 0; j < n - i - 1; j++) {
         if (arr[j].value < arr[j + 1].value) {
           swap(arr, j, j + 1);
         }
 
-        animationItems$.next([...arr]); // Create a new array to trigger change detection
+        this.animationItems$.next([...arr]); // Create a new array to trigger change detection
       }
     }
-
-    return animationItems$;
   }
 }
 
-function generateRandomArray(size: number): SortItem[] {
+function generateRandomArray(): SortItem[] {
   const arr = [];
-  for (let i = 0; i < size; i++) {
+  for (let i = 0; i < 50; i++) {
     arr.push({
       value: Math.floor(Math.random() * 400),
       color: 'white',
